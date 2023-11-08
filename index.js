d2bca18f22a70f07a131d0608441472e1f38242e
atharv-sarode-21 <70423222+atharv-sarode-21@users.noreply.github.com>
2023-11-08 19:52:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let lenght = 60
+let breath = 50
+console.log(lenght*breath)
+
+
+
+let radius = 6
+console.log(3.14*radius**2)
+
+
+
+let prin = 100
+let Rate = 20
+let Time = 5
+
+
+console.log((prin*Rate*Time)/100)
+
+
---
+++ js1.js
@@ -0,0 +1,68 @@
+// function calcPerimeter(l,b){
+//     console.log(2*l*b);
+// }
+// calcPerimeter(2,4)
+
+
+
+// function calcArea(l,b){
+//     console.log(l*b)
+// }
+// calcArea(5,6)
+
+
+
+
+// function calculateCoffeeSupply(age,cups){
+//     total = (age*365)*cups
+//     console.log(total)
+// }
+// calculateCoffeeSupply(21,1)
+
+
+
+
+// function meterToFeet(m){
+
+//     feet = 3.281*m
+//     console.log(feet)
+
+// }
+
+// meterToFeet(1)
+
+
+
+// function FeetTometer(f){
+
+//     m = 0.3048*f
+//     console.log(m)
+
+// }
+// FeetTometer(1)
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "Hello"
+// let str2 ="Atharv"
+
+
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+
+// console.log(1+2+"2")
+
+// let num1
+
+
+// const balance = new Number(100)
+
+// console.log(balance)
+
